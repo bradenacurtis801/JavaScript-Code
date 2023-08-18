e880a275c283e3f1df752632db4cb258961ff24c
bradenacurtis801 <97850331+bradenacurtis801@users.noreply.github.com>
2023-08-18 11:12:05 -0600

    Update passDict.js
    added comments

--- passwordDict/passDict.js
+++ passwordDict/passDict.js
@@ -5,6 +5,7 @@ const OUTPUT_FILE = "./mcupws.json";
 const tick = makeAnimation();
 let pwArr = [];
 
+// Function to create the animation for progress display
 function makeAnimation() {
   const numBars = 50;
   return (currPage, totalPages) => {
@@ -22,10 +23,13 @@ function makeAnimation() {
 (async () => {
   let lastEntry = null;
   try {
+    // Read existing data from the output file if it exists
     const data = await fs.promises.readFile(OUTPUT_FILE, "utf-8");
     pwArr = JSON.parse(data);
     let pwArrLen = pwArr.length;
     lastEntry = pwArrLen > 0 ? pwArrLen : null;
+
+    // Check if the data is already up to date
     if (pwArrLen == 9993) {
       console.log(`\n${OUTPUT_FILE} already up to date.`);
       console.log(
@@ -38,8 +42,10 @@ function makeAnimation() {
     process.stdout.write(`\rScraping...`);
   }
 
+  // Call the animation function to display progress
   tick(1, 100);
 
+  // Launch Puppeteer browser
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const baseUrl = "https://www.passwordrandom.com/most-popular-passwords";
@@ -50,9 +56,10 @@ function makeAnimation() {
     if (lastEntry % 100 === 0) currPage--;
   }
 
-  // Set screen size
+  // Set screen size for the browser
   await page.setViewport({ width: 1080, height: 1024 });
 
+  // Loop through pages and scrape data
   for (; currPage <= 100; currPage++) {
     tick(currPage, 100);
     await page.goto(`${baseUrl}/page/${currPage}`);
@@ -63,22 +70,25 @@ function makeAnimation() {
         if (columns.length === 8) {
           const password = columns[1].innerText;
           if (/^[a-zA-Z0-9]+$/.test(password)) {
-            return password
+            return password;
           }
         }
       }).filter(Boolean); // Remove any undefined elements from the array
-    }, currPage); // pass currPage as an argument to page.evaluate());
+    }, currPage);
+
+    // Loop through passwords on the page and add to the array if valid
     for (let currPw = 0; currPw <= 100; currPw++) {
-        if (result[currPw]) {
-          if (
-            lastEntry === null ||
-            Number(result[currPw]) > Number(lastEntry)
-          ) {
-            pwArr.push(result[currPw])
-          }
+      if (result[currPw]) {
+        if (
+          lastEntry === null ||
+          Number(result[currPw]) > Number(lastEntry)
+        ) {
+          pwArr.push(result[currPw]);
         }
       }
+    }
 
+    // Write data to the output file
     await fs.promises.writeFile(
       OUTPUT_FILE,
       JSON.stringify(pwArr, null, 2),
@@ -86,6 +96,7 @@ function makeAnimation() {
     );
   }
 
+  // Close the browser and finalize
   await browser.close();
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
